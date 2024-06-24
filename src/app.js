cdabb6c0ec6b9d18a2abe1d34dbae6aeffec8cb1
Billy Storm <billstormbiz@gmail.com>
2024-06-24 16:31:56 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -4,8 +4,50 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = [
+  "My dog",
+  "My grandma",
+  "The mailman",
+  "My bird",
+  "my cat",
+  "my pet fish"
+];
+let action = [
+  "ate",
+  "peed on",
+  "crushed",
+  "broke",
+  "threw up on",
+  "threw away"
+];
+let what = [
+  "my homework",
+  "my phone",
+  "my car",
+  "my computer",
+  "my sunglasses"
+];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying",
+  "while i was on the toilet"
+];
+
+function generateExcuse() {
+  let rdmn1 = Math.floor(Math.random() * who.length);
+  let rdmn2 = Math.floor(Math.random() * action.length);
+  let rdmn3 = Math.floor(Math.random() * what.length);
+  let rdmn4 = Math.floor(Math.random() * when.length);
+
+  let excuse = `${who[rdmn1]} ${action[rdmn2]} ${what[rdmn3]} ${when[rdmn4]}`;
+  document.getElementById("excuse").innerHTML = excuse;
+}
+
+window.onload = generateExcuse();
+
+document.getElementById("gen").addEventListener("click", generateExcuse);
